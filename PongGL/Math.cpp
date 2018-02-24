331f7cc0511a0d856c06422b2a311a02078972c6
David Paris <davidparis@davids-macbook-pro.local>
2018-02-24 20:22:56 +0000

    remove extra files

--- PongGL/Math.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Math.cpp
-//  PongGL
-//
-//  Created by David Paris on 11/02/2018.
-//  Copyright © 2018 David Paris. All rights reserved.
-//
-
-#include "Math.hpp"
--- PongGL/Sprite.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Sprite.cpp
-//  PongGL
-//
-//  Created by David Paris on 05/02/2018.
-//  Copyright © 2018 David Paris. All rights reserved.
-//
-
-#include "Sprite.hpp"
